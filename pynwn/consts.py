bc9973dca2d545db77ddfd3bb00926cee0ad4eb1
jmd <jmd2028@gmail.com>
2013-01-02 10:06:33 -0800

    Seperate out some constants for use in other files

---
+++ pynwn/consts.py
@@ -0,0 +1,14 @@
+LANGUAGES = {
+    0: "english",
+    1: "french",
+    2: "german",
+    3: "italian",
+    4: "spanish",
+    5: "polish",
+    128: "korean",
+    129: "chinese_traditional",
+    130: "chinese_simplified",
+    131: "japanese",
+}
+
+GENDER = ['male', 'female']
--- pynwn/tlk.py
+++ pynwn/tlk.py
@@ -1,19 +1,6 @@
 import struct
 
-LANGUAGES = {
-    0: "english",
-    1: "french",
-    2: "german",
-    3: "italian",
-    4: "spanish",
-    5: "polish",
-    128: "korean",
-    129: "chinese_traditional",
-    130: "chinese_simplified",
-    131: "japanese",
-}
-
-GENDER = ['male', 'female']
+from pynwn.consts import *
 
 class Tlk:
     """Loads a TLK file from a file handle.
