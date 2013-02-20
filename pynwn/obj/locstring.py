374c65c954af4131c5e020472e7afea5812c6cf4
jmd <joshua.m.dean@gmail.com>
2013-02-20 11:45:20 -0800

    Add code for modifying CExoLocStrings

--- pynwn/obj/locstring.py
+++ pynwn/obj/locstring.py
@@ -2,15 +2,31 @@ from pynwn.consts import *
 
 class LocString:
     def __init__(self, gff_struct):
-        self.gff = gff_struct[1:]
+        print gff_struct
+        self.gff = gff_struct
 
     def __getitem__(self, lang):
         if not LANGUAGES.has_key(lang):
             raise ValueError("Unknown language type: %d" % lang)
 
         if len(self.gff) == 0: return ''
-        
-        for s in self.gff:
-            if s[0] == lang: return s[1]
+        if len(self.gff) == 1: return self.gff[0]
+
+        temp = self.gff[1:]
+        for s in temp:
+            if s[0] == lang:
+                return s[1]
 
         return ''
+
+    def __setitem__(self, lang, value):
+        if not LANGUAGES.has_key(lang):
+            raise ValueError("Unknown language type: %d" % lang)
+
+        if len(self.gff) == 0: return ''
+        if len(self.gff) == 1: return self.gff[0]
+
+        temp = self.gff[1:]
+        for s in temp:
+            if s[0] == lang:
+                s[1] = value
