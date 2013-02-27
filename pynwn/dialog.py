22e8bd3f201e6381428d45bef2161c8936aeb1a6
jmd <joshua.m.dean@gmail.com>
2013-02-27 00:30:13 -0800

    Redo Dialog object.
    - Moved to property generators.
    - Combine DialogStart and DialogPointer.

--- pynwn/dialog.py
+++ pynwn/dialog.py
@@ -1,121 +1,198 @@
-from pynwn.file.gff import Gff
+from pynwn.file.gff import Gff, make_gff_property, make_gff_locstring_property
+from pynwn.file.gff import GffInstance
+
+class DialogPointer(object):
+    """Pointer to dialog node.
+
+    Note that starting node pointers have been combined with
+    the general dialog node pointer.  They are limited in a couple ways:
+
+    #. No comments.
+    #. They can never be links.
+
+    """
+    def __init__(self, gff, pointer_type, parent, is_start = False):
+        self.gff = gff
+        self.parent = parent
+        self.pointer = pointer_type
+        self._is_start = True
+
+    @property
+    def is_start(self):
+        """Distinguishes starting node pointers from regular node
+        pointers.
+        """
+        return self._is_start
+
+    def get_node(self, i):
+        """Gets a node in the dialog.
+
+        :returns: DialogNode based on pointer type.
+        """
+        if self.pointer == 'RepliesList':
+            return self.parent.replies[i]
+        else:
+            return self.parent.entries[i]
+
+DIALOG_PTR_TABLE = {
+    'script'       : ('Active', 'Text appears when... script'),
+    'index'        : ('Index', 'Index'),
+    'comment'      : ('Comment', 'Comment'),
+    'is_link'      : ('IsChild', 'Linked flag.'),
+    'link_comment' : ('LinkComment', 'Link comment.'),
+}
+
+for key, val in DIALOG_PTR_TABLE.iteritems():
+    setattr(DialogPointer, key, make_gff_property('gff', val))
 
 class DialogNode(object):
-    def __init__(self, gff, pointer_type):
+    """Node in a dialog.
+
+    These are distinguised internally by their pointer types.
+    Entry nodes have reply pointers and vice-versa
+    """
+    def __init__(self, gff, pointer_type, parent):
         self.gff = gff
         self.pointer = pointer_type
-
-    @property
-    def animation(self):
-        return self.gff['Animation']
-
-    @property
-    def animation_loop(self):
-        return self.gff['AnimLoop']
-
-    @property
-    def comment(self):
-        return self.gff['Comment']
-
-    @property
-    def delay(self):
-        return self.gff['Delay']
-
-    @property
-    def script(self):
-        return self.gff['Script']
-
-    @property
-    def sound(self):
-        return self.gff['Sound']
-
-    @property
-    def speaker(self):
-        return self.gff['Speaker']
-
-    @property
-    def text(self):
-        if not self._locstr.has_key('text'):
-            self._locstr['text'] = LocString(self.gff['Text'])
-
-        return self._locstr['text']
-
-    @property
-    def quest(self):
-        return self.gff['Quest']
+        self.parent = parent
 
     @property
     def pointers(self):
-        return [DialogPointer(p, self.pointer) for p in self.gff[self.pointer]]
+        """Dialog pointers
 
+        :returns: List of DialogPointers
+        """
 
-class DialogPointer(object):
-    def __init__(self, gff, pointer_type):
-        self.gff = gff
-        self.pointer = pointer_type
+        result = []
+        i = 0
+        for p in self.gff[self.pointer]:
+            gff_inst = GffInstance(self.gff, self.pointer, i)
+            st_inst  = DialogPointer(gff_inst, self.pointer, self.parent)
+            result.append(st_inst)
+            i += 1
 
-    @property
-    def active(self):
-        return self.gff['Active']
+        return result
 
-    @property
-    def index(self):
-        return self.gff['Index']
+DIALOG_NODE_TABLE = {
+    'animation'      : ('Animation', 'Animation'),
+    'animation_loop' : ('AnimLoop', 'Animation loop'),
+    'comment'        : ('Comment', 'Comment'),
+    'delay'          : ('Delay', 'Delay'),
+    'script'         : ('Script', 'Action attaken script.'),
+    'sound'          : ('Sound', 'Sound'),
+    'speaker'        : ('Speaker', 'Speaker'),
+    'quest'          : ('Quest', 'Quest'),
+}
 
-    @property
-    def is_child(self):
-        return self.gff['IsChild']
+DIALOG_NODE_LOCSTRINGS = {
+    'text' : ('Text', 'Text')
+}
 
-    @property
-    def link_comment(self):
-        return self.gff['LinkComment']
+for key, val in DIALOG_NODE_TABLE.iteritems():
+    setattr(DialogNode, key, make_gff_property('gff', val))
+
+for key, val in DIALOG_NODE_LOCSTRINGS.iteritems():
+    getter, setter = make_gff_locstring_property('gff', val)
+    setattr(getter, '__doc__', val[1])
+    setattr(setter, '__doc__', val[1])
+    setattr(DialogNode, 'get_'+key, getter)
+    setattr(DialogNode, 'set_'+key, setter)
 
 class Dialog(object):
+    """Abstracts .dlg GFF files.
+    """
     def __init__(self, resref, container):
+        self.reply_cache = None
+        self.entry_cache = None
         if resref[-4:] != '.dlg':
+            self._resref = resref
             resref = resref+'.dlg'
+        else:
+            self._resref = resref[:-4]
 
         if container.has_file(resref):
-            self.dlg = container[resref]
-            self.dlg = Gff(self.dlg)
+            self.gff = container[resref]
+            self.gff = Gff(self.gff)
         else:
             raise ValueError("Container does not contain: %s" % resref)
 
-    @property
-    def delay_entry(self):
-        return self.dlg['DelayEntry']
+    def stage(self):
+        """Stages changes to the dialog's GFF structure.
+        """
+        if self.gff.is_loaded():
+            self.container.add_to_saves(self.gff)
 
     @property
-    def delay_entry(self):
-        return self.dlg['DelayEntry']
+    def resref(self):
+        """Resref
+
+        Dialogs don't store their resref internally.
+        """
+        return self._resref
 
     @property
     def entries(self):
-        return [DialogNode(n, 'RepliesList') for n in self.dlg['EntryList']]
+        """Entries
 
-    @property
-    def prevent_zoom(self):
-        return self.dlg['PreventZoomIn']
+        :returns: List of DialogNodes
+        """
+        if self.entry_cache is None:
+            result = []
+            i = 0
+            for p in self.gff['EntryList']:
+                gff_inst = GffInstance(self.gff, 'EntryList', i)
+                st_inst  = DialogNode(gff_inst, 'RepliesList', self)
+                result.append(st_inst)
+                i += 1
+            self.entry_cache = result
+        return self.entry_cache
 
     @property
     def replies(self):
-        return [DialogNode(n, 'EntriesList') for n in self.dlg['ReplyList']]
+        """Replies
 
-    @property
-    def script_abort(self):
-        """Conversation abort script"""
-        return self.dlg['EndConverAbort']
+        :returns: List of DialogNodes
+        """
 
-    @property
-    def script_end(self):
-        """Conversation end script"""
-        return self.dlg['EndConversation']
+        if self.reply_cache is None:
+            result = []
+            i = 0
+            for p in self.gff['ReplyList']:
+                gff_inst = GffInstance(self.gff, 'ReplyList', i)
+                st_inst  = DialogNode(gff_inst, 'EntriesList', self)
+                result.append(st_inst)
+                i += 1
+
+            self.reply_cache = result
+
+        return self.reply_cache
 
     @property
     def starts(self):
-        pass
+        """Starts
 
-    @property
-    def word_count(self):
-        """Conversation word count."""
-        return self.dlg['NumWords']
+        These are limited pointers in the entry list to the
+        topmost level of dialog in a concersation.
+
+        :returns: List of DialogStarts
+        """
+        result = []
+        i = 0
+        for p in self.gff['StartingList']:
+            gff_inst = GffInstance(self.gff, 'StartingList', i)
+            st_inst  = DialogPointer(gff_inst, 'EntriesList', self, True)
+            result.append(st_inst)
+            i += 1
+
+        return result
+
+DIALOG_TABLE = {
+    'delay_entry'  : ('DelayEntry', 'DelayEntry.'),
+    'prevent_zoom' : ('PreventZoomIn', 'No zoom flag.'),
+    'script_abort' : ('EndConverAbort', 'Conversation abort script.'),
+    'script_end'   : ('EndConversation', 'Conversation end script.'),
+    'word_count'   : ('NumWords', 'Word count.'),
+}
+
+for key, val in DIALOG_TABLE.iteritems():
+    setattr(Dialog, key, make_gff_property('gff', val))
