9e15a487666820f881aca44d57950d1f21ef21d0
jmd <joshua.m.dean@gmail.com>
2022-08-09 11:30:47 -0700

    docs: try again

--- docs/conf.py
+++ docs/conf.py
@@ -12,9 +12,9 @@
 #
 import os
 
-# import sys
-#
-# sys.path.insert(0, os.path.abspath("../src/"))
+import sys
+
+sys.path.insert(0, os.path.abspath("../src/pynwn"))
 
 
 # -- Project information -----------------------------------------------------
